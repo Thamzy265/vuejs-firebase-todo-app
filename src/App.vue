acf70d5d7d64d6e68671b9d6608d3a5fbbebb57e
thamzy265 <william5nasoni@gmail.com>
2020-07-28 16:45:16 +0200

    Initialization

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,131 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+      <v-container grid-list-xs>
+        <h3 class="text-primary">Welcome to Task Keeper</h3>
+        <div class="d-flex"> 
+          <v-text-field
+              v-model="todo"
+              label="What are doing today?"
+              @keydown.enter="addTodo"
+            ></v-text-field>
+            <v-btn :disabled="!todo" @click="addTodo" color="success">Add</v-btn>
+        </div>
+           <v-snackbar
+             v-model="infoMsg"
+             :right="true"
+             :top="true"
+             color="error"
+           >
+           Please enter a Task
+          </v-snackbar>
+             
+          <v-card
+            elevation = 10
+          >
+            <v-card-title color="primary" :class="`d-flex justify-space-between`" >
+             
+                <label class="blue--text">Tasks: {{todos.length}}</label>
+                <div>
+                    <label class=" success--text pr-2">Done :</label>
+                    <v-progress-circular :value="progress" color="success">{{tasksDone}}</v-progress-circular>
+                </div>
+              
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-list>
+                <v-list-item  v-for="item in todos" :key="item.id">
+                  <v-list-item-content class="pl-3">
+                    <v-list-item-action>
+                        <v-checkbox
+                          v-model="item.isDone"
+                          @change="todoDone(item)"
+                          :color="item.isDone && 'grey' || 'primary'"
+                          >
+                          <template v-slot:label>
+                              <div
+                                :class="item.isDone && 'text--grey' || 'text--primary'"
+                                class="ml-3"
+                                v-text="item.todo"
+                              ></div>
+                            </template>
+                          </v-checkbox>
+                    </v-list-item-action>
+                      
+                    
+                  </v-list-item-content>
+                  <v-list-item-icon>
+                  <v-icon color="red" @click="deleteTodo(item)">delete</v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+            </v-list>
+             
+            </v-card>
+      </v-container>
+        
+  </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import db from './firebase'
+import firebase from 'firebase'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      todo:"",
+      todos:[],
+      infoMsg:false,
+    }
+  },
+  computed: {
+    tasksDone(){
+      return this.todos.filter(todo => todo.isDone).length
+    },
+    progress(){
+      return this.tasksDone / this.todos.length * 100
+    }
+  },
+  methods: {
+    todoDone(todo){
+    
+   let isDone = todo.isDone ? true : false
+  
+   db.collection('todos')
+        .doc(todo.id)
+        .update({ isDone })
+     
+    },
+    addTodo: function(){
+      if(!this.todo){
+        this.infoMsg = true;
+      }else{
+     
+      db.collection("todos").add({
+          todo: this.todo,
+          isDone: false,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp()
+      })   
+      .catch(function(error) {
+          console.error("Error adding collection: ", error);
+      });
+      this.todo = "";
+      }
+    },
+    deleteTodo(item){
+      db.collection('todos')
+        .doc(item.id)
+        .delete()
+    }
+  },
+  firestore:{
+    todos: db.collection('todos').orderBy('createdAt'),
   }
+ 
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
